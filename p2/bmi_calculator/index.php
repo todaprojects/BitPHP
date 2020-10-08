4cee3fa6edc0af96dda54e6575d0765fe481f166
Tomas Daugela <todaprojects@gmail.com>
2020-10-08 11:05:33 +0300

    modified html & css

--- p2/bmi_calculator/index.php
+++ p2/bmi_calculator/index.php
@@ -7,7 +7,7 @@
     <meta name="author" content="Tomas Daugėla">
     <title>p2 php</title>
     <link rel="stylesheet" href="assets/css/normalize.css">
-    <link rel="stylesheet" href="assets/css/style.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
@@ -17,58 +17,56 @@
         <div class="container">
             <section>
                 <div class="top">BMI calculator</div>
-                <div id="quizBoard">
-                    <div id="result">
-                        <?php
-                        $height = isset($_POST['height']) ? str_replace(',', '.', $_POST['height']) : null;
-                        $weight = isset($_POST['weight']) ? str_replace(',', '.', $_POST['weight']) : null;
+                <div id="result">
+                    <?php
+                    $height = isset($_POST['height']) ? str_replace(',', '.', $_POST['height']) : null;
+                    $weight = isset($_POST['weight']) ? str_replace(',', '.', $_POST['weight']) : null;
 
-                        if (isset($weight) && isset($height)) {
-                            if (is_numeric($height) && is_numeric($weight)) {
-                                $bmi = round($weight / ($height * $height), 2);
-                                switch ($bmi) {
-                                    case ($bmi < 16): {
-                                            echo '<span style="color: red">BMI is ' . $bmi . '<br>Severe Thinness</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 16 && $bmi < 17): {
-                                            echo '<span style="color: orange">BMI is ' . $bmi . '<br>Moderate Thinness</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 17 && $bmi < 18.5): {
-                                            echo '<span style="color: GoldenRod">BMI is ' . $bmi . '<br>Mild Thinness</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 18.5 && $bmi < 25): {
-                                            echo '<span style="color: green">BMI is ' . $bmi . '<br>Normal</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 25 && $bmi < 30): {
-                                            echo '<span style="color: GoldenRod">BMI is ' . $bmi . '<br>Overweight</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 30 && $bmi < 35): {
-                                            echo '<span style="color: red">BMI is ' . $bmi . '<br>Obese Class I</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 35 && $bmi < 40): {
-                                            echo '<span style="color: red">BMI is ' . $bmi . '<br>Obese Class II</span>';
-                                            break;
-                                        }
-                                    case ($bmi > 40): {
-                                            echo '<span style="color: red">BMI is ' . $bmi . '<br>Obese Class III</span>';
-                                            break;
-                                        }
-                                    default: {
-                                            break;
-                                        }
-                                }
-                            } else {
-                                echo 'enter numbers';
+                    if (isset($weight) && isset($height)) {
+                        if (is_numeric($height) && is_numeric($weight)) {
+                            $bmi = round($weight / ($height * $height), 2);
+                            switch ($bmi) {
+                                case ($bmi < 16): {
+                                        echo '<span style="color: red">BMI is ' . $bmi . '<br>Severe Thinness</span>';
+                                        break;
+                                    }
+                                case ($bmi > 16 && $bmi < 17): {
+                                        echo '<span style="color: orange">BMI is ' . $bmi . '<br>Moderate Thinness</span>';
+                                        break;
+                                    }
+                                case ($bmi > 17 && $bmi < 18.5): {
+                                        echo '<span style="color: GoldenRod">BMI is ' . $bmi . '<br>Mild Thinness</span>';
+                                        break;
+                                    }
+                                case ($bmi > 18.5 && $bmi < 25): {
+                                        echo '<span style="color: green">BMI is ' . $bmi . '<br>Normal</span>';
+                                        break;
+                                    }
+                                case ($bmi > 25 && $bmi < 30): {
+                                        echo '<span style="color: GoldenRod">BMI is ' . $bmi . '<br>Overweight</span>';
+                                        break;
+                                    }
+                                case ($bmi > 30 && $bmi < 35): {
+                                        echo '<span style="color: red">BMI is ' . $bmi . '<br>Obese Class I</span>';
+                                        break;
+                                    }
+                                case ($bmi > 35 && $bmi < 40): {
+                                        echo '<span style="color: red">BMI is ' . $bmi . '<br>Obese Class II</span>';
+                                        break;
+                                    }
+                                case ($bmi > 40): {
+                                        echo '<span style="color: red">BMI is ' . $bmi . '<br>Obese Class III</span>';
+                                        break;
+                                    }
+                                default: {
+                                        break;
+                                    }
                             }
+                        } else {
+                            echo 'enter numbers';
                         }
-                        ?>
-                    </div>
+                    }
+                    ?>
                 </div>
             </section>
             <section>
@@ -88,9 +86,8 @@
                     </tbody>
                     <tr>
                         <td>Severe Thinness</td>
-                        <td>< 16</td>
-                    </tr>
-                    <tr>
+                        <td>
+                            < 16</td> </tr> <tr>
                         <td>Moderate Thinness</td>
                         <td>16 - 17</td>
                     </tr>
